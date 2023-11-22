ea91b0cb698ce736c98a616d7d989843eeeeac03
aquarazorda <g32.gelashvili@gmail.com>
2023-11-23 00:11:09 +0400

    added themes & remaps

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+  },
+  {
+    "Shatur/neovim-ayu",
+    config = function()
+      vim.cmd.colorscheme("ayu-mirage")
+    end,
+  },
+  {
+    "sainnhe/gruvbox-material",
+    enabled = true,
+    priority = 1,
+    config = function()
+      vim.g.gruvbox_material_foreground = "material"
+      vim.g.gruvbox_material_background = "hard" -- soft, medium, hard
+      vim.g.gruvbox_material_ui_contrast = "high" -- The contrast of line numbers, indent lines, etc.
+      vim.g.gruvbox_material_float_style = "dim" -- Background of floating windows
+      vim.g.gruvbox_material_statusline_style = "material"
+      vim.g.gruvbox_material_better_performance = 1
+      -- vim.cmd.colorscheme("gruvbox-material")
+    end,
+  },
+}
